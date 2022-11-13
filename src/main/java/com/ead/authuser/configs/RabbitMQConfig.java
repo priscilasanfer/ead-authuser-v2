848fc134669352ae5220f31877871c06234cbf0d
Priscila Ferreira <priscilasanfer@gmail.com>
2022-11-13 19:51:37 -0300

    Criar Publisher e Publicar Event State Transfer (CREATE)

--- src/main/java/com/ead/authuser/configs/RabbitMQConfig.java
+++ src/main/java/com/ead/authuser/configs/RabbitMQConfig.java
@@ -2,15 +2,20 @@ package com.ead.authuser.configs;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.amqp.core.FanoutExchange;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
+
+    @Value(value = "${ead.broker.exchange.userEvent}")
+    private String exchabeUserEvent;
 
     @Autowired
     CachingConnectionFactory cachingConnectionFactory;
@@ -27,6 +32,11 @@ public class RabbitMQConfig {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
         return new Jackson2JsonMessageConverter(objectMapper);
+    }
+
+    @Bean
+    public FanoutExchange fanoutUserEvent(){
+        return new FanoutExchange(exchabeUserEvent);
     }
 
 }
