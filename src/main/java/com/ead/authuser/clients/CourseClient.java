6d63cdc26bcb0d5b7333784b79d2d9996cbba897
Priscila Ferreira <priscilasanfer@gmail.com>
2022-11-21 05:58:25 -0300

    Implementar Retry Resilience4j e Configurar Timeout

--- src/main/java/com/ead/authuser/clients/CourseClient.java
+++ src/main/java/com/ead/authuser/clients/CourseClient.java
@@ -3,11 +3,13 @@ package com.ead.authuser.clients;
 import com.ead.authuser.dtos.CourseDto;
 import com.ead.authuser.dtos.ResponsePageDto;
 import com.ead.authuser.services.UtilsService;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +17,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -31,6 +34,7 @@ public class CourseClient {
     @Value("${ead.api.url.course}")
     String REQUEST_URL_COURSE;
 
+    @Retry(name = "retryInstance", fallbackMethod = "retryFallback")
     public Page<CourseDto> getAllCoursesByUser(UUID userId, Pageable pageable) {
         List<CourseDto> searchResult = null;
 
@@ -59,5 +63,11 @@ public class CourseClient {
     public ResponseEntity<CourseDto> getOneCourseById(UUID courseId) {
         String url = REQUEST_URL_COURSE + "/courses/" + courseId;
         return restTemplate.exchange(url, HttpMethod.GET, null, CourseDto.class);
+    }
+
+    public Page<CourseDto> retryFallback(UUID userId, Pageable pageable, Throwable t){
+        log.error("Inside RetryFallback, cause: {} ", t.toString());
+        List<CourseDto> searchResult = new ArrayList<>();
+        return new PageImpl<>(searchResult);
     }
 }
