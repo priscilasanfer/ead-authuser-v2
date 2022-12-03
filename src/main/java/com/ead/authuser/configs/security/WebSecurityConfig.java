539574a352431082f5a63a2d53700bbb58f4370c
Priscila Ferreira <priscilasanfer@gmail.com>
2022-12-03 11:17:18 -0300

    Authorization com Roles em Microservices

--- src/main/java/com/ead/authuser/configs/security/WebSecurityConfig.java
+++ src/main/java/com/ead/authuser/configs/security/WebSecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -67,5 +69,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return new AuthenticationJwtFilter();
     }
 
+    @Bean
+    public RoleHierarchy roleHierarchy(){
+        RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+        String herarchy = "ROLE_ADMIN > ROLE_INSTRUCTOR \n   ROLE_INSTRUCTOR > ROLE_STUDANT \n ROLE_STUDANT > ROLE_USER";
+        roleHierarchy.setHierarchy(herarchy);
+        return  roleHierarchy;
+    }
 
 }
--- src/main/java/com/ead/authuser/controllers/UserController.java
+++ src/main/java/com/ead/authuser/controllers/UserController.java
@@ -1,5 +1,7 @@
 package com.ead.authuser.controllers;
 
+import com.ead.authuser.configs.security.AuthenticationCurrentUserService;
+import com.ead.authuser.configs.security.UserDetailsImpl;
 import com.ead.authuser.dtos.UserDto;
 import com.ead.authuser.models.UserModel;
 import com.ead.authuser.services.UserService;
@@ -13,6 +15,10 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -40,11 +46,19 @@ public class UserController {
     @Autowired
     UserService userService;
 
+    @Autowired
+    AuthenticationCurrentUserService authenticationCurrentUserService;
+
     @GetMapping
+    @PreAuthorize("hasAnyRole('ADMIN')")
     public ResponseEntity<Page<UserModel>> getAllUsers(
             SpecificationTemplate.UserSpec spec,
-            @PageableDefault(page = 0, size = 10, sort = "userId", direction = Sort.Direction.ASC) Pageable pageable
+            @PageableDefault(page = 0, size = 10, sort = "userId", direction = Sort.Direction.ASC) Pageable pageable,
+            Authentication authentication
     ) {
+
+        UserDetails userDetails= (UserDetailsImpl) authentication.getPrincipal();
+        log.info("Authorization: {}", userDetails.getUsername());
 
         Page<UserModel> userModelPage = userService.findAll(spec, pageable);
 
@@ -63,17 +77,19 @@ public class UserController {
     }
 
     @GetMapping("/{userId}")
+    @PreAuthorize("hasAnyRole('STUDENT')")
     public ResponseEntity<Object> getOneUser(@PathVariable(value = "userId") UUID userId) {
-        Optional<UserModel> userModelOptional = userService.findById(userId);
+        UUID currentUserId = authenticationCurrentUserService.getCurrentUser().getUserId();
 
-        if (userModelOptional.isEmpty()) {
-            return ResponseEntity
-                    .status(HttpStatus.NOT_FOUND)
-                    .body("User not found");
-        } else {
-            return ResponseEntity
-                    .status(HttpStatus.OK)
-                    .body(userModelOptional.get());
+        if(currentUserId.equals(userId)) {
+            Optional<UserModel> userModelOptional = userService.findById(userId);
+            if (!userModelOptional.isPresent()) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found.");
+            } else {
+                return ResponseEntity.status(HttpStatus.OK).body(userModelOptional.get());
+            }
+        }else {
+            throw new AccessDeniedException("Forbidden");
         }
     }
 
