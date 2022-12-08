0d6f2372b43f2bd45ede49906c49dd325d79073d
Priscila Ferreira <priscilasanfer@gmail.com>
2022-12-08 19:42:30 -0300

    Authorization Roles para Instructor

--- src/main/java/com/ead/authuser/controllers/InstructorController.java
+++ src/main/java/com/ead/authuser/controllers/InstructorController.java
@@ -1,13 +1,17 @@
 package com.ead.authuser.controllers;
 
 import com.ead.authuser.dtos.InstructorDto;
+import com.ead.authuser.enums.RoleType;
 import com.ead.authuser.enums.UserType;
+import com.ead.authuser.models.RoleModel;
 import com.ead.authuser.models.UserModel;
+import com.ead.authuser.services.RoleService;
 import com.ead.authuser.services.UserService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -28,7 +32,11 @@ public class InstructorController {
     @Autowired
     UserService userService;
 
+    @Autowired
+    RoleService roleService;
+
     @PostMapping("/subscription")
+    @PreAuthorize("hasAnyRole('ADMIN')")
     public ResponseEntity<Object> saveSubscriptionInstructor(@RequestBody @Valid InstructorDto instructorDto) {
         log.debug("POST saveSubscriptionInstructor instructorDto received: {}", instructorDto.toString());
 
@@ -37,9 +45,15 @@ public class InstructorController {
         if (userModelOptional.isEmpty()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found.");
         } else {
+
+            RoleModel roleModel = roleService.findByRoleName(RoleType.ROLE_INSTRUCTOR)
+                    .orElseThrow(()-> new RuntimeException("Error: Role is not found"));
+
             var userModel = userModelOptional.get();
             userModel.setUserType(UserType.INSTRUCTOR);
             userModel.setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
+            userModel.getRoles().add(roleModel);
+
             userService.updateUser(userModel);
 
             log.debug("POST saveSubscriptionInstructor instructorId saved {} ", userModel.getUserId());
